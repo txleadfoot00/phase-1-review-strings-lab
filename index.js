65e2d6f131914ab6bbd4b43b5c526856a7aabcf9
txleadfoot00 <106290919+txleadfoot00@users.noreply.github.com>
2022-08-11 02:04:37 -0700

    commit tree 1882a614ca34ca20cf8c1e5bd3f55f226451cd4c

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = "Fred Flintstone";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
